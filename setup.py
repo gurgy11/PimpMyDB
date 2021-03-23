8a0d4290faa2ccd4612a084dd48c92140ae2d81b
Gregory Bockus <gregory.bockus@gmail.com>
2021-03-23 14:37:48 -0400

    commit on main

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pimpmydb",
-    version='0.0.1.dev1',
+    version='0.0.8.dev1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
